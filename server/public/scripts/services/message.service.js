f17eef3a6f8119508272b6f9e1aa7c627d28c7bc
ale-cia <ale-cia@users.noreply.github.com>
2017-09-17 19:53:07 -0500

    Connected NASA API

--- server/public/scripts/services/message.service.js
+++ server/public/scripts/services/message.service.js
@@ -2,24 +2,23 @@ myApp.service('MessageService', ['$http', function($http) {
     console.log('Message service loaded.');
 
     var vm = this;
-    vm.messages = { list: [],
-    newMessage: {} 
+    vm.messages = {
+      list: [] ,
+      newMessage: {}
 };
 
     vm.getMessage = function() {
         $http.get('/message').then(function(response) {
-            vm.getMessage.list = response.data;
-
-            console.log('get response: ', self.getMessage);
+            vm.messages.list = response.data;
+            console.log('get response: ', vm.messages);
         });
     };
 
-    vm.addMessage = function(newMessage) { 
-        console.log('going to send this object to the server: ', newMessage);
-               
-        $http.post('/message', self.messages.newMessage).then(function(response) {
+    vm.addMessage = function() {
+        console.log('going to send this object to the server: ', vm.messages.newMessage);
+        $http.post('/message', vm.messages.newMessage).then(function(response) {
             console.log('service post response: ', response);
-            vm.getMessage();            
+            vm.getMessage();
         });
     };
 
@@ -27,18 +26,33 @@ myApp.service('MessageService', ['$http', function($http) {
         console.log('service is going to send this object to the server: ', currentMessage);
         $http.put('/messsage/' + currentMessage._id, currentMessage).then(function(response) {
             console.log('service update response:', response);
-            vm.getMessage();            
+            vm.getMessage();
         });
     };
 
     vm.deleteMessage = function(messageId) {
         console.log('service to delete id: ', messageId);
-        
+
         $http.delete('/message/' + messageId).then(function (response) {
             console.log('service delete response:', response);
             vm.getMessage();
         });
-        
+
     }
-    
+
+    vm.getNasa = function() {
+        $http.get('https://api.nasa.gov/planetary/apod?api_key=BNHQAzvHS60bKNnPfmgV2dlo1qW56zG91aE8Y8Mo').then(function(response) {
+            console.log('get NASA response: ', response.data);
+        });
+    };
+
+    // vm.getNasa();
+
+    vm.getAsteroid = function() {
+        $http.get('https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=BNHQAzvHS60bKNnPfmgV2dlo1qW56zG91aE8Y8Mo').then(function(response) {
+            console.log('get ASTEROID response: ', response.data);
+        });
+    };
+
+    vm.getAsteroid();
 }]);
